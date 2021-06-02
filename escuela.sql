e8b62ec65eba7ac8a24a2eb118168adfcaaf7f64
Cristopher811 <cristcerpug@gmail.com>
2021-06-01 21:40:45 -0500

    aversisirve

--- escuela.sql
+++ escuela.sql
@@ -1,15 +1,20 @@
-CREATE TABLE alumnos{
+CREATE TABLE alumnos(
 	id int not null primary key auto_increment,
 	cuenta varchar(8) not null,
 	nombreCompleto varchar(50) not null,
 	semestre varchar(10) not null
-};
+);
 
-CREATE TABLE calificaciones{
+CREATE TABLE calificaciones(
 	id int not null auto_increment primary key,
 	cuenta varchar(8) not null,
-	nombreCompleto varChar(50) not null,
-	mate varvarChar(50) null,
-	progra int not null
-	parcial int not null
-}
+	nombreCompleto varchar(50) not null,
+	mate varchar(50) null,
+	fisica int not null,
+	ingles int not null,
+	metodologia int not null,
+	progra int not null,
+	analisis int not null,
+	internet int not null,
+	parcial varchar(10) not null
+);
