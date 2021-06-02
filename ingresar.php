a2681d09f57da393d1ac4f8c292f31e7be2847fb
Cristopher811 <cristcerpug@gmail.com>
2021-06-01 21:43:55 -0500

    más cambios

--- ingresar.php
+++ ingresar.php
@@ -2,7 +2,7 @@
 	include("connect.php");
 	$consulta = "INSERT INTO alumnos (cuenta, nombreCompleto, semestre) 
 	VALUES(".$_POST['cuenta'].",'".$_POST['nombre']."','".$_POST['semestre']."')";
-  $consulta2 = "INSERT INTO calificaciones (cuenta, nombreCompleto, mate, fisica, ingles, metodologia, progra,analisis,internet, parcial) values(".$POST['cuenta'].",'".$_POST['nombre']."','".$_POST['Mate']."','".$_POST['Fisica']."','".$_POST[Ingles]."', '".$_POST[Metodo]."','".$_POST[Progra]."','".$_POST[Analisis]."','".$_POST[Internet]"')";
+  $consulta2 = "INSERT INTO calificaciones (cuenta, nombreCompleto, mate, fisica, ingles, metodologia, progra,analisis,internet, parcial) values(".$POST['cuenta'].",'".$_POST['nombre']."','".$_POST['Mate']."','".$_POST['Fisica']."','".$_POST[Ingles]."','".$_POST[Metodo]."','".$_POST[Progra]."','".$_POST[Analisis]."','".$_POST[Internet]."')";
 	mysqli_query($conex, $consulta, $consulta2);
 	echo "Sísirve";
 ?>
