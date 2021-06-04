972d714281e29d340ef0cec4be179aa7fc781b1a
Cristopher811 <cristcerpug@gmail.com>
2021-06-03 20:38:13 -0500

    ya sirve2

--- eliminar.php
+++ eliminar.php
@@ -1,9 +1,10 @@
 <?php
 	include("connect.php");
 	$consulta = "DELETE FROM alumnos WHERE cuenta=".$_POST['cuenta']; 
-	$consulta2 = "DELETE FROM calificaciones WHERE cuenta =".$_POST['cuenta']. "AND parcial =".$_POST['parcial'];
+	$consulta2 = "DELETE FROM calificaciones WHERE cuenta='".$_POST['cuenta']. "' AND parcial='" . $_POST['parcial'] . "'";
 	mysqli_query($conex, $consulta);
 	mysqli_query($conex, $consulta2);
 	print_r($_POST);
+	echo $consulta2;
 	echo "Eliminado";
 ?>
