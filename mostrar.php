d9066a5c009a4838e7783d0d14759b8575396bad
Cristopher811 <cristcerpug@gmail.com>
2021-06-05 13:37:55 -0500

    mostrar

--- mostrar.php
+++ mostrar.php
@@ -9,13 +9,49 @@
 	<header>
 		<nav>
 			<ul class="nav_links">
-				<li><a href="form_modificar.php">Modificar</a></li>
+				<li><a href="index.php">Ingresar</a></li>
 				<li><a href="form_eliminar.php">Eliminar</a></li>
-				<li><a href="mostrar.php">Ver alumnos</a></li>
+				<li><a href="form_modificar.php">Modificar</a></li>
 			</ul>
 			<a class="contacto"	href="#"><Button>Sobre nosotros</Button></a>
 		</nav>
 	</header>
-	
+	<table class="datatable">
+		<thead>
+			<tr>
+				<th>Número de Cuenta</th>
+				<th>Alumno</th>
+				<th>Parcial</th>
+				<th>Matemáticas VI</th>
+				<th>Física IV</th>
+				<th>Metodología de la Investigación</th>
+				<th>Programación IV</th>
+				<th>Análisis y Diseño de sistemas</th>
+				<th>Programación de Internet II</th>
+			</tr>
+		</thead>
+<?php
+		include("connect.php");
+		$consulta = "select * from calificaciones order by nombreCompleto ASC";
+		$resultado = mysqli_query($conex, $consulta);
+		while($datos=mysqli_fetch_array($resultado)){
+			echo "
+						<tbody>
+						<tr>
+							<td>".$datos['cuenta']."</td>
+							<td>".$datos['nombreCompleto']."</td>
+							<td>".$datos['parcial']."</td>
+							<td>".$datos['mate']."</td>
+							<td>".$datos['fisica']."</td>
+							<td>".$datos['metodologia']."</td>
+							<td>".$datos['progra']."</td>
+							<td>".$datos['analisis']."</td>
+							<td>".$datos['internet']."</td>
+						</tr>
+					</tbody>
+					";
+		}
+?>	
+	</table>
 </body>
 </html>
